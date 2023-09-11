727e42e22a8714bba76f8da5ad219fa67a09912d
Favourkorede <106892255+Favourkorede@users.noreply.github.com>
2023-09-11 22:28:47 +0100

    Create Script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// To get Current Week day
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const currentDate = new Date();
+let day = weekday[currentDate.getDay()];
+document.getElementById("CurrentDay").innerHTML = day;
+
+
+// To get Current UTC Time
+const CurrentUTCtime = new Date(currentDate);
+let time = CurrentUTCtime.getTime();
+let hours=CurrentUTCtime.getUTCHours();
+const minutes=CurrentUTCtime.getUTCMinutes();
+const Currenttime=`${hours}:${minutes<10?"0" : ""}${minutes}`;
+
+document.getElementById("CurrentUTCtime").innerHTML = Currenttime;
